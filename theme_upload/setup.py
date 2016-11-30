69fc613f9447ee474e1423b3ed34e3bcfcce0740
cwen0708 <cwen0708@gmail.com>
2016-11-30 19:04:05 +0800

    樣式工具更新
    theme_upload\upload.py 為 theme_upload.py 的副本

---
+++ theme_upload/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Created with YooLiang Technology (侑良科技).
+# Author: Qi-Liang Wen (温啓良）
+# Web: http://www.yooliang.com/
+# Date: 2016/11/26
+
+
+from distutils.core import setup
+import py2exe
+import sys
+import certifi
+
+print certifi.where()
+sys.argv.append('py2exe')
+setup(
+    options={'py2exe': dict(bundle_files=1, optimize=2)},
+    console=["upload.py"],
+    includes=["OpenSSL", "certifi"],
+    zipfile=None,
+    data_files=["cacert.pem"],
+    ascii=True,
+    dll_excludes=['msvcr71.dll'],
+    compressed=True,
+)
